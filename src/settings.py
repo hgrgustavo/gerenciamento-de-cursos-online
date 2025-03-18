ed8645333c96a65e914453b32701263f60d2f528
gus <gustavohgrdev@gmaiil.com>
2025-03-18 18:04:50 -0300

    fix: add admin table, fix db diagram

--- src/settings.py
+++ src/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "db_crud_schema",
         "USER": "root",
-        "PASSWORD": "admin",
+        "PASSWORD": "Gustavo@mysql69",
         "HOST": "localhost",
         "PORT": "3306",
     }
--- website/models.py
+++ website/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 
+class Administrador(models.Model):
+    nome = models.CharField(max_length=45)
+    senha = models.CharField(max_length=45)
+
+    class Meta:
+        
+        db_table = 'administrador'
+
+
 class Alunos(models.Model):
-    nome = models.CharField(max_length=255, default="")
-    email = models.CharField(unique=True, max_length=255, default="")
-    telefone = models.CharField(max_length=18, default="")
+    nome = models.CharField(max_length=255)
+    email = models.CharField(unique=True, max_length=255)
+    telefone = models.CharField(max_length=18)
 
     class Meta:
         
@@ -12,10 +21,10 @@ class Alunos(models.Model):
 
 
 class Cursos(models.Model):
-    nome = models.CharField(max_length=255, default="")
+    nome = models.CharField(max_length=255)
     descricao = models.TextField()
     carga_horaria = models.IntegerField()
-    instrutor = models.CharField(max_length=255, default="")
+    instrutor = models.CharField(max_length=255)
 
     class Meta:
         
@@ -29,4 +38,3 @@ class Inscricoes(models.Model):
     class Meta:
         
         db_table = 'inscricoes'
-        unique_together = (('cursos', 'alunos'),)
